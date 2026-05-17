a48d90711d6cd49ac881322ac617dd9d8769b9e4
Shubham Dubey <me@shb.am>
2026-05-17 23:29:58 +0530

    Add Homebrew shellenv to Darwin zsh profile

--- home/darwin/default.nix
+++ home/darwin/default.nix
@@ -2,4 +2,10 @@
   imports = [
     ./iterm2.nix
   ];
+
+  programs.zsh.profileExtra = ''
+    if [[ -x /opt/homebrew/bin/brew ]]; then
+      eval "$(/opt/homebrew/bin/brew shellenv)"
+    fi
+  '';
 }
